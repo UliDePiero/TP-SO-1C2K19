9798b3a89cb2f7c1cacffebc90fc22856ba26dab
pepeciavirella <udepiero@alumno.huergo.edu.ar>
2019-04-14 19:36:07 -0300

    Memoria Config

--- Memoria/Memoria.c
+++ Memoria/Memoria.c
@@ -4,5 +4,55 @@
  *  Created on: 12 abr. 2019
  *      Author: utnso
  */
+#include "Memoria.h"
+void configurar(ConfiguracionMemoria* configuracion) {
+
+	char* campos[] = {
+					   "PUERTO",
+					   "IP_FS",
+					   "PUERTO_FS",
+					   "IP_SEEDS",
+					   "PUERTO_SEEDS",
+					   "RETARDO_MEM",
+					   "RETARDO_FS",
+					   "TAM_MEM",
+					   "RETARDO_JOURNAL",
+					   "RETARDO_GOSSIPING",
+					   "MEMORY_NUMBER=1"
+					 };
+
+	t_config* archivoConfig = archivoConfigCrear(RUTA_CONFIG, campos);
+
+	//Relleno los campos con la info del archivo
+
+	strcpy(configuracion->PUERTO, archivoConfigSacarStringDe(archivoConfig, "PUERTO"));
+	strcpy(configuracion->IP_FS, archivoConfigSacarStringDe(archivoConfig, "IP_FS"));
+	strcpy(configuracion->PUERTO_FS, archivoConfigSacarStringDe(archivoConfig, "PUERTO_FS"));
+	strcpy(configuracion->IP_SEEDS, archivoConfigSacarStringDe(archivoConfig, "IP_SEEDS"));
+	strcpy(configuracion->PUERTO_SEEDS, archivoConfigSacarStringDe(archivoConfig, "PUERTO_SEEDS"));
+	configuracion->RETARDO_MEM = archivoConfigSacarIntDe(archivoConfig, "RETARDO_MEM");
+	configuracion->RETARDO_FS = archivoConfigSacarIntDe(archivoConfig, "RETARDO_FS");
+	configuracion->TAM_MEM = archivoConfigSacarIntDe(archivoConfig, "TAM_MEM");
+	configuracion->RETARDO_JOURNAL = archivoConfigSacarIntDe(archivoConfig, "RETARDO_JOURNAL");
+	configuracion->RETARDO_GOSSIPING = archivoConfigSacarIntDe(archivoConfig, "RETARDO_GOSSIPING");
+	configuracion->MEMORY_NUMBER = archivoConfigSacarIntDe(archivoConfig, "MEMORY_NUMBER");
+
+	archivoConfigDestruir(archivoConfig);
+}
+int main()
+{
+	configuracion = malloc(sizeof(ConfiguracionMemoria));
+	configurar(configuracion);
+
+	//servidor
+	//FUNCIONES SOCKETS (Usar dependiendo de la biblioteca que usemos)
+	//socketEscucha= levantarServidorIPautomatica(configuracion->PUERTO, BACKLOG); //BACKLOG es la cantidad de clientes que pueden conectarse a este servidor
+	//socketActivo = aceptarComunicaciones(socketEscucha);
+	free(configuracion);
+
+	cerrarSocket(socketActivo);
+	cerrarSocket(socketEscucha);
+}
+
 
 
--- Memoria/Memoria.h
+++ Memoria/Memoria.h
@@ -9,5 +9,58 @@
 #define MEMORIA_H_
 
 
+//Ruta del archivo de configuraciones
+#define RUTA_CONFIG "LFS.config"
+#define BACKLOG 5 //Creo que se pueden conectar solo 2 clientes pero hay que verificar
+
+#include <stdio.h>
+
+#include <openssl/md5.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <commons/config.h>
+#include <commons/log.h>
+#include <arpa/inet.h>
+
+///---------------------VARIABLES A UTILIZAR-------------------------
+int socketEscucha;
+int	socketActivo;
+
+///---------------------ESTRUCTURA DE CONFIGURACION DE LFS-------------------------
+
+//Estructura para datos del archivo de configuracion
+typedef struct {
+	char PUERTO[10];
+	char IP_FS[20];
+	char PUERTO_FS[10];
+	char IP_SEEDS[100]; //Verificar
+	char PUERTO_SEEDS[100]; //Verificar
+	int RETARDO_MEM;
+	int RETARDO_FS;
+	int TAM_MEM;
+	int RETARDO_JOURNAL;
+	int RETARDO_GOSSIPING;
+	int MEMORY_NUMBER;
+} ConfiguracionMemoria;
+/*
+PUERTO=8001
+IP_FS="172.0.0.1"
+PUERTO_FS=5003
+IP_SEEDS=["172.0.0.2","172.0.0.3"]
+PUERTO_SEEDS=[8000,8001]
+RETARDO_MEM=600
+RETARDO_FS=600
+TAM_MEM=2048
+RETARDO_JOURNAL=60000
+RETARDO_GOSSIPING=30000
+MEMORY_NUMBER=1
+*/
+//Estructura para guardar la configuracion del proceso
+ConfiguracionMemoria* configuracion;
+
+void journalization();
 
 #endif /* MEMORIA_H_ */
