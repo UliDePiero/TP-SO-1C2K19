fda5f144e207060f07f3d0810577f5ebc3bf019b
Juan Paulo Vispo <juanpaulovispo@yahoo.com.ar>
2019-04-18 22:06:28 -0300

    Librería de sockets y estructura protocolo

---
+++ Shared/src/log.h
@@ -0,0 +1,12 @@
+#ifndef LIB2LOG_H_
+#define LIB2LOG_H_
+
+#include "../commons/log.h"
+#include "../commons/string.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+	t_log* logInit(char** argv, char* who);
+
+#endif
---
+++ Shared/src/protocolo.h
@@ -0,0 +1,48 @@
+
+
+#ifndef PROTOCOLO_H_
+#define PROTOCOLO_H_
+
+#include <stdlib.h>
+#include <stdint.h> //para los "int8_t" "int16_t" "int32_t"
+#include <string.h>
+
+#define MAX_BUFFER 1024
+
+
+typedef struct {
+	int8_t  type;
+	int16_t length;
+} __attribute__ ((__packed__)) tHeader;
+
+typedef struct {
+	int8_t  type;
+	int16_t length;
+	char    payload[MAX_BUFFER];
+} __attribute__ ((__packed__)) tPaquete;
+
+
+typedef enum {
+
+	/* Mensajes comunes */
+	DESCONEXION,
+	NO_SE_OBTIENE_RESPUESTA,
+	NADA
+} tMensaje;
+
+
+
+/*
+ * Aca se definen los payloads que se van a mandar en los paquetes
+ */
+
+typedef struct {
+	uint32_t timestamp;
+	uint16_t  key;
+	char * value;
+}tRegistro;
+
+
+
+
+#endif /* PROTOCOLO_H_ */
