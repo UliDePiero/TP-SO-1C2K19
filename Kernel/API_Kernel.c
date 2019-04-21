f562ff3f4eee18ed70442ebc975b60c69d5c2ea7
isarasibar <sarasibar.ignacio@gmail.com>
2019-04-21 05:01:58 -0300

    Fix API Liss

--- Kernel/API_Kernel.c
+++ Kernel/API_Kernel.c
@@ -6,61 +6,70 @@
  */
 #include "API_Kernel.h"
 
+
+
 int main(void){
-	char *linea;
+	char* linea;
+	char** comando;
 	linea = readline(">");
-
 	while(1){
-		char** comando = string_n_split(string_trim(linea), 6, " ");
-		if(string_starts_with("SELECT ", comando[0])){
+		string_trim(&linea);
+		if(string_starts_with(linea, "SELECT ")){
+			comando = string_n_split(linea, 3, " ");
 			if(comandoValido(3, comando)){
-
+				printf("comando valido\n");
 			}
 		}
-		else if(string_starts_with("INSERT ",comando[0])){
+		else if(string_starts_with(linea, "INSERT")){
+			comando = string_n_split(linea, 4, " ");
 			if(comandoValido(4, comando)){
-
+				printf("comando valido\n");
 			}
 		}
-		else if(string_starts_with("CREATE ",comando[0])){
+		else if(string_starts_with(linea, "CREATE")){
+			comando = string_n_split(linea, 5, " ");
 			if(comandoValido(5, comando)){
-
+				printf("comando valido\n");
 			}
 		}
-		else if(string_starts_with("DESCRIBE ",comando[0]) ){
+		else if(string_starts_with(linea, "DESCRIBE") ){
+			comando = string_n_split(linea, 2, " ");
 			if(comandoValido(2, comando)){
-
+				printf("comando valido\n");
 			}
 		}
-		else if(string_starts_with("DROP ",comando[0])){
+		else if(string_starts_with(linea, "DROP")){
+			comando = string_n_split(linea, 2, " ");
 			if(comandoValido(2, comando)){
-
+				printf("comando valido\n");
 			}
 		}
-		else if(string_starts_with("JOURNAL ",comando[0])){
+		else if(string_starts_with(linea, "JOURNAL")){
 			if(comandoValido(1, comando)){
-
+				printf("comando valido\n");
 			}
 		}
-		else if(string_starts_with("ADD ",comando[0])){
+		else if(string_starts_with(linea, "ADD")){
+			comando = string_n_split(linea, 5, " ");
 			if(comandoValido(5, comando)){
-
+				printf("comando valido\n");
 			}
 		}
-		else if(string_starts_with("RUN ",comando[0])){
+		else if(string_starts_with(linea, "RUN")){
+			comando = string_n_split(linea, 2, " ");
 			if(comandoValido(2, comando)){
-
+				printf("comando valido\n");
 			}
 		}
-		else if(string_starts_with("METRICS ",comando[0])){
+		else if(string_starts_with(linea, "METRICS")){
 			if(comandoValido(1, comando)){
-
+				printf("comando valido\n");
 			}
 		}
-		else{
+		else if(!string_is_empty(linea)){
 			informarComandoInvalido();
 		}
-		free(comando);
+		//free(comando);
 		free(linea);
 		linea = readline(">");
 	}
@@ -68,14 +77,25 @@ int main(void){
 
 
 int comandoValido(int inputs, char** comando){
+	/*printf("comando[0]: %s \n", comando[0]);
+	printf("comando[1]: %s \n", comando[1]);
+	printf("comando[2]: %s \n", comando[2]);
+	printf("comando[3]: %s \n", comando[3]);*/
 	int valido = 1;
+
 	for(int i = 1; i<inputs; i++){
-		if(string_is_empty(comando[i])){
+		if(string_is_empty(&comando[i])){
+			printf("entro %d \n", i);
 			valido = 0;
+			break;
 		}
 	}
-	if(!string_is_empty(comando[inputs])){
-		valido = 0;
+	if(valido){
+		char** subComando = string_n_split(comando[inputs-1], 2, " ");
+		if(!string_is_empty(&subComando[1])){
+			valido = 0;
+		}
+		free(subComando);
 	}
 
 	if(!valido){
@@ -85,5 +105,5 @@ int comandoValido(int inputs, char** comando){
 }
 
 void informarComandoInvalido(){
-	printf("ERROR: Comando no valido");
+	printf("ERROR: Comando no valido \n");
 }
--- Kernel/API_Kernel.h
+++ Kernel/API_Kernel.h
@@ -8,6 +8,23 @@
 #ifndef API_KERNEL_H_
 #define API_KERNEL_H_
 
+
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+#include <openssl/md5.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <commons/config.h>
+#include <commons/log.h>
+#include <arpa/inet.h>
+#include <sockets.h>
+#include <configuraciones.h>
+#include <commons/string.h>
+
 int comandoValido(int inputs, char** comando);
 void informarComandoInvalido();
 
