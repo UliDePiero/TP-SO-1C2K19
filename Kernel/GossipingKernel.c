c7585197d0dff2d4f9c1ba2a9623f04da2146823
juanmaconstenla <juanmaconstenla@gmail.com>
2019-07-18 05:38:20 -0300

    Algunos avances de Gossiping para Kernel

--- Kernel/GossipingKernel.c
+++ Kernel/GossipingKernel.c
@@ -7,6 +7,64 @@
 
 #include "Planificador.h"
 
+int pideRetardoGossiping() {
+	tPaquete* msjeEnviado = malloc(sizeof(tPaquete));
+	msjeEnviado->type = HANDSHAKE;
+	strcpy(msjeEnviado->payload, "Pedido de RETARDO_GOSSIPING a Memoria");
+	msjeEnviado->length = sizeof(msjeEnviado->payload);
+	enviarPaquete(socketMemoria, msjeEnviado, logger, "Kernel realiza pedido de Retardo de Gossiping a Memoria");
+	liberarPaquete(msjeEnviado);
+
+	tPaquete* msjeRecibido = malloc(sizeof(tPaquete));
+	recv(socketMemoria, msjeRecibido, sizeof(tPaquete), 0);
+	int retardoGossiping = atoi(msjeRecibido->payload);
+	liberarPaquete(msjeRecibido);
+
+	return retardoGossiping;
+}
+
+void armarNodoMemoria(TablaGossip* nodo) {
+	// Cargo datos faltantes del nodo
+	//nodo->socketMemoria = -1;
+	nodo->criterioSC = 0;
+	nodo->criterioSHC = 0;
+	nodo->criterioEC = 0;
+	// Si el nodo no está en listaGossiping, lo agrego. Sino, lo libero
+	if (!nodoEstaEnLista(listaGossiping, nodo))
+		list_add(listaGossiping, nodo);
+	else
+		free(nodo);
+}
+
+void pideListaGossiping(int socketMem) {
+	tPaquete* msjeEnviado = malloc(sizeof(tPaquete));
+	msjeEnviado->type = GOSSIPING;
+	strcpy(msjeEnviado->payload, "Kernel pide Lista de Gossiping a Memoria");
+	msjeEnviado->length = sizeof(msjeEnviado->payload);
+	enviarPaquete(socketMem, msjeEnviado, logger, "Kernel realiza pedido de Lista de Gossiping a Memoria");
+	liberarPaquete(msjeEnviado);
+
+	int status;
+	int cantElementosListaRecibida;
+
+	recv(socketMem, &cantElementosListaRecibida, sizeof(int), 0);
+
+	for (int i = 0; i < cantElementosListaRecibida; i++) {
+		TablaGossip* nodoRecibido = malloc(sizeof(TablaGossip));
+		status = recibirNodoYDeserializar(nodoRecibido, socketMem);
+		if (status)
+			armarNodoMemoria(nodoRecibido);
+	}
+}
+
+void gossipingKernel() {
+	// Pide a la Memoria conectada por Archivo de Configuración el Retardo de Gossiping
+	int retardoGossiping = pideRetardoGossiping();
+	// Pide a la memoria del archivo de configuración la Lista de Gossiping
+	pideListaGossiping(socketMemoria);
+
+}
+
 int recibirNodoYDeserializar(TablaGossip *nodo, int socketMem) {
 	int status;
 
