39c252f666b06e4463b26db3d7fae808deb21995
GastonMauro <gastonmauro7@gmail.com>
2019-07-19 16:56:33 -0300

    Subo makefile

---
+++ LFS/Debug/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../API_LFS.c \
+../FileSystem.c \
+../Lissandra.c 
+
+OBJS += \
+./API_LFS.o \
+./FileSystem.o \
+./Lissandra.o 
+
+C_DEPS += \
+./API_LFS.d \
+./FileSystem.d \
+./Lissandra.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"/home/utnso/workspace/tp-2019-1c-GGWP/BiblioCompartida" -I"/home/utnso/workspace/tp-2019-1c-GGWP/BiblioCompartida/Debug" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
