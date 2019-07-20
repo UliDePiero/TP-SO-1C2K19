6527b8316f8e1c1712d20c57599f65448ee30f5a
pepeciavirella <udepiero@alumno.huergo.edu.ar>
2019-07-20 02:12:04 -0300

    saco debug

--- Kernel/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lBiblioCompartida -lpthread -lcommons -lreadline
-
